db204d26afb33f445747fe42314044a8eec6128e
Hamza Ashraf <hamzamirza658@gmail.com>
2022-09-30 21:40:59 +0500

    404

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const router = useRouter()
+  const handleRoute = () => {
+    router.push('/product')
+  }
   return (
     <div className={styles.container}>
       Home Page
@@ -13,6 +18,7 @@ export default function Home() {
       <h1>
         <Link href="/product">Product</Link>
       </h1>
+      <button onClick={handleRoute}>Place Your Order</button>
     </div>
   )
 }
